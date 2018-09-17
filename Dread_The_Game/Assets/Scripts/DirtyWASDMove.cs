44bb25459f979ad7e39d688691bf158f26618215
Rune Barrett <runebarrett@gmail.com>
2018-09-17 20:06:02 +0200

    add very basic movement and a camera follow script

---
+++ Dread_The_Game/Assets/Scripts/DirtyWASDMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirtyWASDMove : MonoBehaviour
+{
+    public float speed = 20f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            pos.z += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.z -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+
+        transform.position = pos;
+    }
+}
---
+++ Dread_The_Game/Assets/Scripts/FollowCam.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+
+    public Transform target;
+
+    [Header("Distance")]
+    public float distance = 18f;
+    public float minDistance = 14f;
+    public float maxDistance = 22f;
+    public Vector3 offset;
+
+    [Header("Smoothing/Speeds")]
+    public float smooth = 8f;
+    public float scrollSens = 10f;
+
+
+    void Start()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        if (!target) { print("No camera target set for " + gameObject.name); return; }
+
+        float num = Input.GetAxis("Mouse ScrollWheel");
+        distance -= num * scrollSens;
+        distance = Mathf.Clamp(distance, minDistance, maxDistance);
+
+        Vector3 pos = target.position + offset;
+        pos -= transform.forward * distance;
+
+        transform.position = Vector3.Lerp(transform.position, pos, smooth * Time.deltaTime);
+    }
+}
