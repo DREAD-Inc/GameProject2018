0254279d6338da3f1080213dfd47cd978450dc0f
Rune Barrett <runebarrett@gmail.com>
2018-09-19 17:04:45 +0200

    add initial scripts for shooting

---
+++ Dread_The_Game/Assets/Scripts/CharacterAttributes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAttributes : MonoBehaviour {
+
+	public string name;
+	public string health;
+	public float shootRange;
+	public GameObject weapon;
+
+
+	
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Dread_The_Game/Assets/Scripts/Shoot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+	private float range;
+	public Transform projectile;
+	// Use this for initialization
+	void Start () {
+		//if type==enemy
+		//if type==player
+		range = GetComponent<CharacterAttributes>().shootRange;
+		//projectile = GetComponent<CharacterAttributes>().weapon.projectile;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
