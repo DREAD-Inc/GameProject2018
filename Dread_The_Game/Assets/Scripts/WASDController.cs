997931d9813737b1063e2ec73b30ef898de6d2c8
Arash <arash.abedin@gmail.com>
2018-09-19 22:17:11 +0200

    add jumping

--- Dread_The_Game/Assets/Scripts/WASDController.cs
+++ Dread_The_Game/Assets/Scripts/WASDController.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class WASDController : MonoBehaviour
 {
 
-    public float speed = 6;
+    public float speed = 8f;
+    public float Gravity = 300f;
     private Rigidbody rBody;
     private Camera cam;
     private Vector3 input;
     private Vector3 inputArrow;
     private Vector3 velocity;
-
     private Vector3 lookDir; //default instantiated as (0,0,0)
+    private bool onGround;
 
     void Start()
     {
@@ -23,6 +24,9 @@ public class WASDController : MonoBehaviour
 
     void Update()
     {
+        //Enabaling Gravity to the player
+        rBody.AddForce(Vector3.down * Gravity * rBody.mass);
+
         input = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
         inputArrow = new Vector3(Input.GetAxisRaw("HorizontalA"), 0f, Input.GetAxisRaw("VerticalA"));
 
@@ -38,6 +42,11 @@ public class WASDController : MonoBehaviour
         }
         else if (inputArrow.normalized != Vector3.zero) lookDir = inputArrow.normalized;
         else if (input.normalized != Vector3.zero) lookDir = input.normalized;
+
+
+       
+   
+   
     }
 
     void FixedUpdate()
@@ -49,5 +58,24 @@ public class WASDController : MonoBehaviour
 
         rBody.rotation = rotation;
         rBody.velocity = velocity;
+
+         //Jumping
+        if(onGround)
+            {
+                if( Input.GetButton("Jump"))
+                {
+                    rBody.velocity = new Vector3(0f, 200f , 0f);
+                    onGround = false;
+                }
+            }
     }
+
+    // Enablaling player to jump again as soon as it hitted ground or a surface
+    void OnCollisionEnter(Collision other)
+        {
+        if(other.gameObject.CompareTag("GroundPlane") || other.gameObject.CompareTag("Suface"))
+            {
+             onGround = true;
+            }
+        }
 }
