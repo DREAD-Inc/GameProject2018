933c67bdb6e029514f034f6982f18bb038454e77
Arash <arash.abedin@gmail.com>
2018-09-20 20:53:31 +0200

    sample networking. getting message from server

--- Dread_The_Game/Assets/Scripts/Controller.cs
+++ Dread_The_Game/Assets/Scripts/Controller.cs
@@ -9,14 +9,32 @@ public class Controller : MonoBehaviour {
 	void Start () {
 		
 		StartCoroutine(ConnectToServer());
+		socket.On("USER_CONNECTED", OnUserConnected );
+		socket.On("PLAY", OnUserPlay );
+
 
 	}
 	IEnumerator ConnectToServer(){
 		yield return new WaitForSeconds(0.5f);
+
 	 	socket.Emit("USER_CONNECT");
+
+		yield return new WaitForSeconds(1f);
+
+		//Sample data for testing
+		Dictionary<string,string> data = new Dictionary<string,string>();
+		data["name"] = "Fuckerman";
+		Vector3 position = new Vector3(0,0,0);
+		data["position"] = position.x + " , " + position.y + " , " + position.z;
+
+		socket.Emit("PLAY", new JSONObject(data));
 	}
-	// Update is called once per frame
-	void Update () {
-		
+
+	private void OnUserConnected( SocketIOEvent evt){
+		Debug.Log("The message from server is "+ evt.data );
+	}
+	
+	private void OnUserPlay( SocketIOEvent evt){
+		Debug.Log("The message from server is "+ evt.data );
 	}
 }
