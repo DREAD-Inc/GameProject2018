62391e0766bea9eecc37de9a94094443ab101097
Rune Barrett <runebarrett@gmail.com>
2018-09-20 21:17:59 +0200

    add _ in fromt of our folders to separate them from plugins etc

---
+++ Dread_The_Game/Assets/_Scripts/Controller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class Controller : MonoBehaviour {
+ public SocketIOComponent socket;
+	// Use this for initialization
+	void Start () {
+		
+		StartCoroutine(ConnectToServer());
+		socket.On("USER_CONNECTED", OnUserConnected );
+		socket.On("PLAY", OnUserPlay );
+
+
+	}
+	IEnumerator ConnectToServer(){
+		yield return new WaitForSeconds(0.5f);
+
+	 	socket.Emit("USER_CONNECT");
+
+		yield return new WaitForSeconds(1f);
+
+		//Sample data for testing
+		Dictionary<string,string> data = new Dictionary<string,string>();
+		data["name"] = "Fuckerman";
+		Vector3 position = new Vector3(0,0,0);
+		data["position"] = position.x + " , " + position.y + " , " + position.z;
+
+		socket.Emit("PLAY", new JSONObject(data));
+	}
+
+	private void OnUserConnected( SocketIOEvent evt){
+		Debug.Log("The message from server is "+ evt.data );
+	}
+	
+	private void OnUserPlay( SocketIOEvent evt){
+		Debug.Log("The message from server is "+ evt.data );
+	}
+}
