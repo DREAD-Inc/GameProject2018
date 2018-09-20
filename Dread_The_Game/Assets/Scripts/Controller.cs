d3a1d02be1222548b3cf767094061dd8b90ba45f
Arash <arash.abedin@gmail.com>
2018-09-20 20:24:07 +0200

    Emiting USER_CONNECT to server

---
+++ Dread_The_Game/Assets/Scripts/Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class Controller : MonoBehaviour {
+ public SocketIOComponent socket;
+	// Use this for initialization
+	void Start () {
+		
+		StartCoroutine(ConnectToServer());
+
+	}
+	IEnumerator ConnectToServer(){
+		yield return new WaitForSeconds(0.5f);
+	 	socket.Emit("USER_CONNECT");
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
