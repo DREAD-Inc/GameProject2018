3ba7c1012687bd6b7b7dce9013d7f8150ec1685f
Arash <arash.abedin@gmail.com>
2018-09-29 18:21:32 +0200

    Make globes for reptileWeapon to move forward towards the aimed position

--- Dread_The_Game/Assets/_Scripts/Weapons/ReptileWeapon.cs
+++ Dread_The_Game/Assets/_Scripts/Weapons/ReptileWeapon.cs
@@ -5,19 +5,42 @@ using UnityEngine;
 public class ReptileWeapon : Weapon
 {
 
+
+    public GlobeController globe;
+    public float globeSpeed;
+
+    public float timeBetweenShots;
+    private float shotCounter;
+
+    public Transform firePoint;
+
     void Start()
     {
-        reptileBall = transform.Find("ReptileBall");
+          //firePoint =  transform.Find("GameObject");
+          //reptileBall = transform.Find("ReptileBall");
+          //globe = GetComponent<GlobeController>();
     }
 
     void Update()
     {
-        if (isShooting) Shoot();
-        else reptileBall.gameObject.SetActive(false);
+        if (isShooting){
+            
+            shotCounter -= Time.deltaTime;
+
+            if(shotCounter <= 0){
+                shotCounter = timeBetweenShots;
+                Debug.Log(firePoint);
+                GlobeController newGlobe = Instantiate(globe,firePoint.position,firePoint.rotation) as GlobeController;
+                newGlobe.speed = globeSpeed;
+            }
+        }else {
+            shotCounter = 0;
+        }
+      //  else reptileBall.gameObject.SetActive(false);
     }
 
-    protected override void Shoot()
-    {
-        reptileBall.gameObject.SetActive(true);
-    }
+    // protected override void Shoot()
+    // {
+    //     reptileBall.gameObject.SetActive(true);
+    // }
 }
