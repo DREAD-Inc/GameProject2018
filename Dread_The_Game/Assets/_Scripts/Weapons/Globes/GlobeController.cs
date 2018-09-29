f4f87c6a8a258a8787f7ca3ac6f8ea7166b0bf7f
Arash <arash.abedin@gmail.com>
2018-09-30 00:37:38 +0200

    move transform.position to FixedUpdate

--- Dread_The_Game/Assets/_Scripts/Weapons/Globes/GlobeController.cs
+++ Dread_The_Game/Assets/_Scripts/Weapons/Globes/GlobeController.cs
@@ -8,13 +8,9 @@ public class GlobeController : MonoBehaviour {
 	public GameObject targetCharachter;
 	Vector3 newVector;
 	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    void FixedUpdate(){
 		newVector = stupidBestFirstSearch(targetCharachter);
-		//transform.Translate(newVector * speed * Time.deltaTime);
 		transform.position += newVector;
 	}
 	void OnCollisionEnter (Collider other)
